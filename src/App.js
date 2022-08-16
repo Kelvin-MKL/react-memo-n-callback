8ceb6da15e908fd1e83db4f5ddd65d376c0961d1
Kelvin-MKL <109009865+Kelvin-MKL@users.noreply.github.com>
2022-08-16 20:04:46 +1000

    useMemo example created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useMemo } from "react";
+
+function Square({ color }) {
+  console.log("Square func is called.");
+  return (
+    <div style={{ height: "50px", width: "50px", background: color }}></div>
+  );
+}
 
 function App() {
+  const [renderCounter, setRenderCounter] = useState(0);
+  const [color, setColor] = useState("blue");
+
+  const squareMemo = useMemo(() => {
+    console.log("SquareMemo is called.");
+    return <Square color={color}></Square>;
+  }, [color]);
+
+  const renderMessage = `The entire app is rendered ${renderCounter} times.`;
+  console.log(renderMessage);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div>
+        <button
+          style={{ height: "auto", width: "200px" }}
+          onClick={() => setRenderCounter(renderCounter + 1)}
         >
-          Learn React
-        </a>
-      </header>
+          Render App
+        </button>
+        <h4>{renderMessage}</h4>
+      </div>
+      <div>
+        {squareMemo}
+        <button
+          style={{ marginTop: "30px" }}
+          onClick={() => setColor(color === "blue" ? "red" : "blue")}
+        >
+          Change Color
+        </button>
+      </div>
     </div>
   );
 }
